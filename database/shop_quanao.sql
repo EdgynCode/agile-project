92b9d2d24c574876513c593158c97a0c1bd6d635
TONG HUYNH MINH NHAT <4701104151@student.hcmue.edu.vn>
2024-04-22 22:29:11 +0700

    testing1

--- database/shop_quanao.sql
+++ database/shop_quanao.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 20, 2022 lúc 10:28 AM
+-- Phiên bản máy phục vụ: 10.4.24-MariaDB
+-- Phiên bản PHP: 8.1.6
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
